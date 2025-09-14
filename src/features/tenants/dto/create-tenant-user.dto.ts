a7edcbad525762d8a64e2e0af6c474019e7e0a65
Nadir <tellainadir@gmail.com>
2025-09-14 09:15:03 +0100

    tenant management

--- src/features/tenants/dto/create-tenant-user.dto.ts
+++ src/features/tenants/dto/create-tenant-user.dto.ts
@@ -1,5 +1,12 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { IsEmail, IsNotEmpty, IsString, Matches, MaxLength, MinLength } from 'class-validator';
+import {
+  IsEmail,
+  IsNotEmpty,
+  IsString,
+  Matches,
+  MaxLength,
+  MinLength,
+} from 'class-validator';
 
 export class CreateTenantUserDto {
   @ApiProperty({
